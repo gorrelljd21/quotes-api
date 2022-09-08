b782290659cc379ca5170f9fb2bb8a3176efdfe7
Jessica Gorrell <gorrell.jd@c02vcan5htd6.localdomain>
2022-09-08 11:25:10 -0400

    WIP: imports block doesn't like the gin import and []quote has red squiggly, trouble shooting

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Quote struct {
+	Quote string `json:"quote"`
+}
+
+var quotes = []quote{
+	{Author: "Rob Pike", Quote: "Don't communicate by sharing memory, share memory by communicating."},
+	{Author: "Rob Pike", Quote: "Concurrency is not parallelism."},
+	{Author: "Rob Pike", Quote: "Channels orchestrate; mutexes serialize."},
+	{Author: "Rob Pike", Quote: "The bigger the interface, the weaker the abstraction."},
+	{Author: "Dmitri Shuralyov", Quote: "Use consistent spelling of certain words."},
+	{Author: "Dmitri Shuralyov", Quote: "Single spaces between spaces."},
+	{Author: "Dmitri Shuralyov", Quote: "Avoid unused method receiver names."},
+	{Author: "Dmitri Shuralyov", Quote: "Comments for humans always have a single space after the slashes."},
+}
+
+func main() {
+	r := gin.Default()
+	r.GET("/quotes", getQuote())
+	r.Run("localhost:8080")
+}
+
+func getQuote(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, quotes)
+}
