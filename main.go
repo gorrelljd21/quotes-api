155fe349602cc57746fd76551fa260bc7fba67ff
Jessie <gorrell.jd@pg.com>
2022-09-29 15:23:00 -0400

    made a change

--- main.go
+++ main.go
@@ -8,7 +8,6 @@ import (
 	"os"
 
 	"database/sql"
-	// _ "database/sql"
 
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
@@ -30,7 +29,7 @@ var db *sql.DB
 func main() {
 	err := databaseConnection()
 	if err != nil {
-		log.Fatalln(err)
+		log.Println(err)
 	}
 
 	r := gin.Default()
@@ -45,13 +44,13 @@ func databaseConnection() error {
 	mustGetenv := func(dns string) string {
 		gettingEnv := os.Getenv(dns)
 		if gettingEnv == "" {
-			log.Fatalf("Warning: %s environment variable not set", dns)
+			log.Printf("Warning: %s environment variable not set", dns)
 		}
 		return gettingEnv
 	}
 
 	var (
-		dbUser         = os.Getenv("DB_USER") //gorrelljd21
+		dbUser         = os.Getenv("DB_USER") //postgres
 		dbPwd          = mustGetenv("DB_PWD")
 		dbName         = mustGetenv("DB_NAME")              //quotes_database
 		unixSocketPath = mustGetenv("INSTANCE_UNIX_SOCKET") // /cloudsql/jessie-apprentice:us-central1:quotes-database
@@ -99,14 +98,18 @@ func getRandomQuote(c *gin.Context) {
 }
 
 func getQuoteByIdSQL(c *gin.Context) {
-	id := c.Param("id")
-	row := db.QueryRow(fmt.Sprintf("select id, phrase, author from quotes where id = '%s'", id))
-	q := &quote{}
-	err := row.Scan(&q.ID, &q.Quote, &q.Author)
-	if err != nil {
-		log.Fatal(err)
+	if manageHeader(c) {
+		id := c.Param("id")
+		row := db.QueryRow(fmt.Sprintf("select id, phrase, author from quotes where id = '%s'", id))
+		q := &quote{}
+		err := row.Scan(&q.ID, &q.Quote, &q.Author)
+		if err != nil {
+			log.Println(err)
+		}
+		c.JSON(http.StatusOK, q)
+	} else if !manageHeader(c) {
+		c.JSON(http.StatusUnauthorized, gin.H{"message": "unauthorized"})
 	}
-	c.JSON(http.StatusOK, q)
 }
 
 // func getQuoteById(c *gin.Context) {
@@ -156,7 +159,7 @@ func addQuote(c *gin.Context) {
 var mapOfQuotes = map[string]quote{
 	"0d949b68-6b04-4b35-82e5-63159b7608f8": {ID: "0d949b68-6b04-4b35-82e5-63159b7608f8", Quote: "Don't communicate by sharing memory, share memory by communicating.", Author: "Rob Pike"},
 	"f9a7ab3a-9fc5-40b3-8c2e-76239ca037ce": {ID: "f9a7ab3a-9fc5-40b3-8c2e-76239ca037ce", Quote: "Concurrency is not parallelism.", Author: "Rob Pike"},
-	"a240c7e9-1570-4c36-ae5f-699e4cb5e4d7": {ID: "a240c7e9-1570-4c36-ae5f-699e4cb5e4d7", Quote: "The bigger the interface, the weaker the abstraction.", Author: "Rob Pike"},
+	// "a240c7e9-1570-4c36-ae5f-699e4cb5e4d7": {ID: "a240c7e9-1570-4c36-ae5f-699e4cb5e4d7", Quote: "The bigger the interface, the weaker the abstraction.", Author: "Rob Pike"},
 	"a2523b46-42d4-42f6-aeb9-42da4b928c4a": {ID: "a2523b46-42d4-42f6-aeb9-42da4b928c4a", Quote: "Use consistent spelling of certain words.", Author: "Dmitri Shuralyov"},
 	"f5a05e7f-1e71-462f-8036-9b7c8bfbed65": {ID: "f5a05e7f-1e71-462f-8036-9b7c8bfbed65", Quote: "Single spaces between spaces.", Author: "Dmitri Shuralyov"},
 	"7dbde6f1-c411-40ca-af84-cc7fec7c06ec": {ID: "7dbde6f1-c411-40ca-af84-cc7fec7c06ec", Quote: "Avoid unused method receiver names.", Author: "Dmitri Shuralyov"},
