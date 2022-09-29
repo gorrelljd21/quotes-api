0ad3240b539375339cfa3f9faf1ce70fb8cea116
gorrelljd21 <92065426+gorrelljd21@users.noreply.github.com>
2022-09-29 15:23:34 -0400

    Revert "added dockerfile and troubleshot"

--- main.go
+++ main.go
@@ -100,9 +100,9 @@ func getRandomQuote(c *gin.Context) {
 
 func getQuoteByIdSQL(c *gin.Context) {
 	id := c.Param("id")
-	row := db.QueryRow(fmt.Sprintf("select id, phrase, author from quotes where id = '%s'", id))
+	row := db.QueryRow("select id, phrase, author from quotes where id = %s", id)
 	q := &quote{}
-	err := row.Scan(&q.ID, &q.Quote, &q.Author)
+	err := row.Scan(q.ID, q.Author, q.Quote)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -156,6 +156,7 @@ func addQuote(c *gin.Context) {
 var mapOfQuotes = map[string]quote{
 	"0d949b68-6b04-4b35-82e5-63159b7608f8": {ID: "0d949b68-6b04-4b35-82e5-63159b7608f8", Quote: "Don't communicate by sharing memory, share memory by communicating.", Author: "Rob Pike"},
 	"f9a7ab3a-9fc5-40b3-8c2e-76239ca037ce": {ID: "f9a7ab3a-9fc5-40b3-8c2e-76239ca037ce", Quote: "Concurrency is not parallelism.", Author: "Rob Pike"},
+	"1f9c3697-5232-45a8-82b7-ba9ac5f0799c": {ID: "1f9c3697-5232-45a8-82b7-ba9ac5f0799c", Quote: "Channels orchestrate; mutexes serialize.", Author: "Rob Pike"},
 	"a240c7e9-1570-4c36-ae5f-699e4cb5e4d7": {ID: "a240c7e9-1570-4c36-ae5f-699e4cb5e4d7", Quote: "The bigger the interface, the weaker the abstraction.", Author: "Rob Pike"},
 	"a2523b46-42d4-42f6-aeb9-42da4b928c4a": {ID: "a2523b46-42d4-42f6-aeb9-42da4b928c4a", Quote: "Use consistent spelling of certain words.", Author: "Dmitri Shuralyov"},
 	"f5a05e7f-1e71-462f-8036-9b7c8bfbed65": {ID: "f5a05e7f-1e71-462f-8036-9b7c8bfbed65", Quote: "Single spaces between spaces.", Author: "Dmitri Shuralyov"},
