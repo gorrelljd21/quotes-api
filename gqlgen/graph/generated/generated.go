0f0569f5050efa9fbd4e94565a2b203aba41c912
Jessie <gorrell.jd@pg.com>
2022-10-18 11:48:44 -0400

    created delete mutation in schema and resolvers generated successfully

--- gqlgen/graph/generated/generated.go
+++ gqlgen/graph/generated/generated.go
@@ -44,6 +44,7 @@ type DirectiveRoot struct {
 
 type ComplexityRoot struct {
 	Mutation struct {
+		DeleteQuote func(childComplexity int, id string) int
 		InsertQuote func(childComplexity int, input model.NewQuote) int
 	}
 
@@ -61,6 +62,7 @@ type ComplexityRoot struct {
 
 type MutationResolver interface {
 	InsertQuote(ctx context.Context, input model.NewQuote) (*model.Quote, error)
+	DeleteQuote(ctx context.Context, id string) (*model.Quote, error)
 }
 type QueryResolver interface {
 	Quote(ctx context.Context) (*model.Quote, error)
@@ -81,6 +83,18 @@ func (e *executableSchema) Complexity(typeName, field string, childComplexity in
 	ec := executionContext{nil, e}
 	_ = ec
 	switch typeName + "." + field {
+
+	case "Mutation.deleteQuote":
+		if e.complexity.Mutation.DeleteQuote == nil {
+			break
+		}
+
+		args, err := ec.field_Mutation_deleteQuote_args(context.TODO(), rawArgs)
+		if err != nil {
+			return 0, false
+		}
+
+		return e.complexity.Mutation.DeleteQuote(childComplexity, args["id"].(string)), true
 
 	case "Mutation.insertQuote":
 		if e.complexity.Mutation.InsertQuote == nil {
@@ -224,6 +238,7 @@ input newQuote {
 
 type Mutation {
     insertQuote(input: newQuote!): Quote
+    deleteQuote(id: String!): Quote
 }`, BuiltIn: false},
 }
 var parsedSchema = gqlparser.MustLoadSchema(sources...)
@@ -231,6 +246,21 @@ var parsedSchema = gqlparser.MustLoadSchema(sources...)
 // endregion ************************** generated!.gotpl **************************
 
 // region    ***************************** args.gotpl *****************************
+
+func (ec *executionContext) field_Mutation_deleteQuote_args(ctx context.Context, rawArgs map[string]interface{}) (map[string]interface{}, error) {
+	var err error
+	args := map[string]interface{}{}
+	var arg0 string
+	if tmp, ok := rawArgs["id"]; ok {
+		ctx := graphql.WithPathContext(ctx, graphql.NewPathWithField("id"))
+		arg0, err = ec.unmarshalNString2string(ctx, tmp)
+		if err != nil {
+			return nil, err
+		}
+	}
+	args["id"] = arg0
+	return args, nil
+}
 
 func (ec *executionContext) field_Mutation_insertQuote_args(ctx context.Context, rawArgs map[string]interface{}) (map[string]interface{}, error) {
 	var err error
@@ -369,6 +399,66 @@ func (ec *executionContext) fieldContext_Mutation_insertQuote(ctx context.Contex
 	}()
 	ctx = graphql.WithFieldContext(ctx, fc)
 	if fc.Args, err = ec.field_Mutation_insertQuote_args(ctx, field.ArgumentMap(ec.Variables)); err != nil {
+		ec.Error(ctx, err)
+		return
+	}
+	return fc, nil
+}
+
+func (ec *executionContext) _Mutation_deleteQuote(ctx context.Context, field graphql.CollectedField) (ret graphql.Marshaler) {
+	fc, err := ec.fieldContext_Mutation_deleteQuote(ctx, field)
+	if err != nil {
+		return graphql.Null
+	}
+	ctx = graphql.WithFieldContext(ctx, fc)
+	defer func() {
+		if r := recover(); r != nil {
+			ec.Error(ctx, ec.Recover(ctx, r))
+			ret = graphql.Null
+		}
+	}()
+	resTmp, err := ec.ResolverMiddleware(ctx, func(rctx context.Context) (interface{}, error) {
+		ctx = rctx // use context from middleware stack in children
+		return ec.resolvers.Mutation().DeleteQuote(rctx, fc.Args["id"].(string))
+	})
+	if err != nil {
+		ec.Error(ctx, err)
+		return graphql.Null
+	}
+	if resTmp == nil {
+		return graphql.Null
+	}
+	res := resTmp.(*model.Quote)
+	fc.Result = res
+	return ec.marshalOQuote2ᚖgithubᚗcomᚋgorrelljd21ᚋquotesᚑstarterᚋgqlgenᚋgraphᚋmodelᚐQuote(ctx, field.Selections, res)
+}
+
+func (ec *executionContext) fieldContext_Mutation_deleteQuote(ctx context.Context, field graphql.CollectedField) (fc *graphql.FieldContext, err error) {
+	fc = &graphql.FieldContext{
+		Object:     "Mutation",
+		Field:      field,
+		IsMethod:   true,
+		IsResolver: true,
+		Child: func(ctx context.Context, field graphql.CollectedField) (*graphql.FieldContext, error) {
+			switch field.Name {
+			case "id":
+				return ec.fieldContext_Quote_id(ctx, field)
+			case "quote":
+				return ec.fieldContext_Quote_quote(ctx, field)
+			case "author":
+				return ec.fieldContext_Quote_author(ctx, field)
+			}
+			return nil, fmt.Errorf("no field named %q was found under type Quote", field.Name)
+		},
+	}
+	defer func() {
+		if r := recover(); r != nil {
+			err = ec.Recover(ctx, r)
+			ec.Error(ctx, err)
+		}
+	}()
+	ctx = graphql.WithFieldContext(ctx, fc)
+	if fc.Args, err = ec.field_Mutation_deleteQuote_args(ctx, field.ArgumentMap(ec.Variables)); err != nil {
 		ec.Error(ctx, err)
 		return
 	}
@@ -2585,6 +2675,12 @@ func (ec *executionContext) _Mutation(ctx context.Context, sel ast.SelectionSet)
 
 			out.Values[i] = ec.OperationContext.RootResolverMiddleware(innerCtx, func(ctx context.Context) (res graphql.Marshaler) {
 				return ec._Mutation_insertQuote(ctx, field)
+			})
+
+		case "deleteQuote":
+
+			out.Values[i] = ec.OperationContext.RootResolverMiddleware(innerCtx, func(ctx context.Context) (res graphql.Marshaler) {
+				return ec._Mutation_deleteQuote(ctx, field)
 			})
 
 		default:
--- gqlgen/graph/schema.resolvers.go
+++ gqlgen/graph/schema.resolvers.go
@@ -17,7 +17,6 @@ import (
 
 // AddQuote is the resolver for the addQuote field
 func (r *mutationResolver) InsertQuote(ctx context.Context, input model.NewQuote) (*model.Quote, error) {
-
 	quote := &model.Quote{
 		Quote:  input.Quote,
 		Author: input.Author,
@@ -43,12 +42,17 @@ func (r *mutationResolver) InsertQuote(ctx context.Context, input model.NewQuote
 	return quote, nil
 }
 
+// DeleteQuote is the resolver for the deleteQuote field.
+func (r *mutationResolver) DeleteQuote(ctx context.Context, id string) (*model.Quote, error) {
+	panic(fmt.Errorf("not implemented: DeleteQuote - deleteQuote"))
+}
+
 // Quote is the resolver for the quote field.
 func (r *queryResolver) Quote(ctx context.Context) (*model.Quote, error) {
 	var randQuote *model.Quote
 
 	request, err := http.NewRequest("GET", "http://34.160.90.176:80/quote", nil)
-	request.Header.Set("x-api-key", "COCKTAILSAUCE")
+	request.Header.Set("X-Api-Key", "COCKTAILSAUCE")
 
 	if err != nil {
 		return nil, err
@@ -74,7 +78,7 @@ func (r *queryResolver) Quote(ctx context.Context) (*model.Quote, error) {
 // QuoteID is the resolver for the quoteId field.
 func (r *queryResolver) QuoteID(ctx context.Context, id string) (*model.Quote, error) {
 	request, err := http.NewRequest("GET", fmt.Sprintf("http://34.160.90.176:80/quote/%s", id), nil)
-	request.Header.Set("x-api-key", "COCKTAILSAUCE")
+	request.Header.Set("X-Api-Key", "COCKTAILSAUCE")
 
 	if err != nil {
 		return nil, err
